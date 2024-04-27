94419387f602a465efcf8cd9ad1e7f0e2f714846
doyeon <kim2004on@gmail.com>
2024-04-27 10:36:58 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,28 +1,36 @@
-num = 100
-
-#2차원 배열 선언
-dp = [[0] * 10 for _ in range(num + 1)]
+#쉬운 계단 수
+#DP
 
 count = int(input())
 
-#초깃값 세팅
+MAX = 100
+DIVIDE = 1000000000
+
+#2차원 배열 선언
+dp = [[0] * 10 for _ in range(MAX + 1)]
+
+#1일때 세팅
 for i in range(1, 10):
     dp[1][i] = 1
 
-
+#마지막 자리 수 기준
 for i in range(2, 100+1):
-    for j in range(9+1):
+    for j in range(9+1): #
+        
         if j == 0:
-            dp[i][j] = dp[i-1][1] % 1000000000
+            dp[i][j] = dp[i-1][1] % DIVIDE # 0일때 앞자리가 -1이므로, 뒷자리인 1
+            
         elif j == 9:
-            dp[i][j] = dp[i-1][8] % 1000000000
+            dp[i][j] = dp[i-1][8] % DIVIDE # 9일때 뒷자리가 10이므로
+            
         else:
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1] % 1000000000
+            dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % DIVIDE #dp에 저장된 그 전의 값의 j기준 앞뒤 값을 더해준다.
+                                                            #왜? 계단수라는 것이 인접한 수랑 1차이가 나므로
+            
 sum = 0
-#결과 출력
+#결과 출력 - 총 경우의 수를 구하기 때문에
 for i in range(0, 10):
-    sum = (sum + dp[count][i]) % 1000000000
+    sum = (sum + dp[count][i]) % DIVIDE
 
-
-print(sum % 1000000000  )
+print(sum % DIVIDE)
     
