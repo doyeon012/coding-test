f31d6899aa99630d3707b12375530ac91d1abda6
김도연 <kim2004on@gmail.com>
2025-01-25 19:22:00 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 192 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,40 +1,44 @@
-data = input()
+word_input = input()
 
 stack = []
 result = ''
-check = False # 괄호안의 여부를 체크
+check = False
 
-for d in data:
+
+for word in word_input:
   
-    # < 이거 만났을때 기존에 쌓였던 뒤집혀야 할 값 뒤집기
-    if d == '<':
-        check = True
-        for _ in range(len(stack)):
-            result += stack.pop()
-
-    # <> 이거 이외에 값들은 여기에 걸림.
-    stack.append(d)
+  # 괄호 만나면 기존에 쌓였던 뒤집혀야 할 값 뒤집기
+  if word == '<':
+    check = True
     
-    # <> 내부에 있었던 값 순차적 처리
-    if d == '>':
-        check = False
-        result += ''.join(stack)
-        stack = []    
-            
-            
-    # 공백 만났을때 <이거와 같이 쌓였던 뒤집혀야 할 값 뒤집기
-    # 그치만 i=0이다? 그럼 끝내야 함.
-    if d == ' ' and check == False:
-        for i in range(len(stack)):
-            if i == 0:
-                stack.pop()
-                continue
-            result += stack.pop()
-        result += ' '
-        
-# 스택에 값이 남아있는 경우는 괄호의 경우가 아니기에 역으로 추가합니다.
+    for _ in range(len(stack)):
+      result += stack.pop()
+  
+  # 보통 값들은 여기서 걸려서 스택에 쌓임
+  stack.append(word)
+  
+  # 괄호 끝났기에 False로 바꾸고 ''.join 이용해서 바로 한번에 넣어주고 스택 비우기
+  if word == '>':
+    check = False
+    
+    result += ''.join(stack)
+    stack = []
+  
+  # 공백 만났어? 그럼 < 이거 만났을때와 동일하게 스택에 있는 값들 비우기
+  # 근데 스택이 비었어? 그럼 끝!
+  if word == ' ' and check == False:
+    
+    for i in range(len(stack)):
+      if i == 0:
+        stack.pop()
+        continue
+      result += stack.pop()
+      
+    result += ' '
+
+# 스택에 잔존하는 값들이 괄호의 경우가 아니기에 pop 이용해서 역으로 result에 넣기
 if stack:
     for _ in range(len(stack)):
-        result += stack.pop()
-
+      result += stack.pop()
+      
 print(result)
