8781125d62d7c207b32c6e9ad9e75a4cceca540b
김도연 <kim2004on@gmail.com>
2025-01-28 18:54:48 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 1228 ms, Memory: 33948 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -13,8 +13,10 @@ prohibited = set(arr)
 
 count = 0 
 
+# combinations 이용해서 3개로 조합 뽑아내고
 for comb in combinations(range(1, N + 1), 3):
   
+ # 3개뽑아낸거에 또 2개 뽑아내서 not any 이용해서 피해야 할 조합이 a, b, 혹은 b, a다 들어가 있지 않아야! 피했다는 거니?
  if not any((a, b) in prohibited or (b, a) in prohibited for a, b in combinations(comb, 2)):
         count += 1
 
