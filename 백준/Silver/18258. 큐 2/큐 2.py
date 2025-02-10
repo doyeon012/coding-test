7bbd70d825006704ce1bcc6564beaf4cdff9799e
김도연 <kim2004on@gmail.com>
2025-02-10 15:35:11 +0900

    [Silver IV] Title: 큐 2, Time: 1408 ms, Memory: 144336 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,42 +1,35 @@
-#큐2
-
-# push X: 정수 X를 큐에 넣음
-# pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-# size: 큐에 들어있는 정수의 개수 출력.
-# empty: 큐가 비어있으면 1, 아니면 0을 출력.
-# front: 큐의 가장 앞에 있는 정수를 출력. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-# back: 큐의 가장 뒤에 있는 정수를 출력. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-
-# 시간복잡도가 O(1)로 이뤄져야 시간 초과x > 그렇기 때문에 deque 사용
 import sys
 from collections import deque
 
-count = int(sys.stdin.readline())
+input = sys.stdin.readline
+
+N = int(input())
+
 queue = deque()
 
-for i in range(count):
-    command = sys.stdin.readline().split()
-
-    if command[0] == "push":
-        queue.append(command[1])
-
-    elif command[0] == "pop":
+for i in range(N):
+    comm = input().split()
+    
+    if comm[0] == 'push':
+        queue.append(comm[1]) 
+    
+    elif comm[0] == 'pop':
         if not queue:
             print(-1)
         else:
-            print(queue.popleft()) # 가장 먼저 들어간 것 제거
+            print(queue.popleft()) # 큐에서 가장 앞에 있는 정수 출력
     
-    elif command[0] == "size":
+    elif comm[0] == 'size':
         print(len(queue))
-
-    elif(command[0] == "empty"):
-        if not queue:
+    
+    elif comm[0] == 'empty':
+        if not queue: # queue가 비었을때 1출력 아니면 0 출력
             print(1)
         else:
             print(0)
     
-    elif(command[0] == "front"):
-        if not queue:
+    elif comm[0] == 'front':
+        if not queue: # 큐가 애초에 비었으면 -1, queue[0] 이게 가장 앞에 있는 정수 출력
             print(-1)
         else:
             print(queue[0])
@@ -45,5 +38,4 @@ for i in range(count):
         if not queue:
             print(-1)
         else:
-            print(queue[-1])
-        
+            print(queue[-1])    
