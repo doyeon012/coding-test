46d6c978f773dd2c5b799e712261a2af021f05c6
김도연 <kim2004on@gmail.com>
2025-03-17 22:57:18 +0900

    [Silver III] Title: 후위 표기식2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
@@ -2,26 +2,27 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+
 equation = input().strip()
 num_list = [int(input()) for _ in range(N)]
 
 stack = []
-    
+
 for i in equation:
-    if 'A' <= i <= 'Z':
-        stack.append(num_list[ord(i) - ord('A')])
-    # 연산자 만날 경우
-    else:
-        str2 = stack.pop()
-        str1 = stack.pop() # 2개를 꺼내서 연산하고서!
-        
-        # 다스 스택에 넣어줌.
-        if i == '+':
-            stack.append(str1 + str2)
-        elif i == '-':
-            stack.append(str1 - str2)
-        elif i == '*':
-            stack.append(str1 * str2)
-        elif i == '/':
-            stack.append(str1 / str2)
-print(f'{stack[0]:.2f}')  
+  if 'A' <= i <= 'Z':
+    stack.append(num_list[ord(i) - ord('A')])
+  
+  else:
+    str2 = stack.pop()
+    str1 = stack.pop()
+    
+    if i == '+':
+      stack.append(str1 + str2)
+    elif i == '-':
+      stack.append(str1 - str2)
+    elif i == '*':
+      stack.append(str1 * str2)
+    elif i == '/':
+      stack.append(str1 / str2)
+
+print(f'{stack[0]:.2f}')
