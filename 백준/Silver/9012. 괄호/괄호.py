4b292171aec2967479b673455ae62db540902c39
김도연 <kim2004on@gmail.com>
2025-03-17 17:52:25 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,21 @@
+N = int(input())
+
+for i in range(N):
+  stack = []
+  target = input()
+  
+  for j in target:
+    if j == '(':
+      stack.append(j)
+    elif j == ')':
+      if len(stack) != 0:
+        stack.pop()
+      else:
+        print('NO')
+        break
+  
+  else:
+    if len(stack) == 0:
+      print('YES')
+    else:
+      print('NO')
