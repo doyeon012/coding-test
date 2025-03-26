590a1ab10fe3e65958aa174f800684499b4cd57c
Doyeon Kim <kim2004on@gmail.com>
2025-03-26 13:58:03 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,20 +1,18 @@
-#2*n 타일링
-#DP
-import sys
-input = sys.stdin.readline
+N = int(input())
 
-#초기값 세팅
-num = int(input())
-arr = [0, 1, 2] 
 
-for i in range(3, num+1):
-    arr.append(arr[i-1] + arr[i-2])
-    #(2*1크기를 끝에 더해줌. n-1의 최대 방법의 수) + 
-    # (1*2크기 2개를 끝에 더해줌. n-2의 최대 방법의 수)
-    
-    #주어진 타일이 2개 > (n-1의 가짓수에 끝에 2*1 붙여준것) +
-    # (n-2의 가짓수에 끝에 1*2 2개 붙여준 것)
-    
-    #붙였다고해서 경우의 수가 달라지진 않고, n-1의 가짓수 + n-2의 가짓수
-    
-print(arr[num] % 10007) # 문제에서 10,007로 나눈 나머지 출력
+DP = [0] * (N + 1)
+if N >= 1:
+  DP [1] = 1
+if N >= 2:
+  DP[2] = 2
+if N >= 3:
+  DP[3] = 3
+
+
+for i in range(4, N + 1):
+  DP[i] = DP[i - 1] + DP[i - 2]
+  
+  
+result = DP[N] % 10007
+print(result)
